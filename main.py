b293f2288d185a304eb44b0a9a8d08e7256a887f
DESKTOP-B684D4A\mkonj <painstar325@gmail.com>
2022-06-02 01:28:25 +0900

    주소, 매개변수

--- main.py
+++ main.py
@@ -1,12 +1,65 @@
 from fastapi import FastAPI
+from enum import Enum
+from pydantic import BaseModel
+from typing import Union
 
 app = FastAPI()
-my_awesome_api = FastAPI()
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}, {"item_name": "Baz2"}]
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@my_awesome_api.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10, cnt: int= None):
+    if cnt:
+        return cnt
+    return fake_items_db[skip : skip + limit]
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.dict()}
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
+
